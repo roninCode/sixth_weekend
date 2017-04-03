62e3cc406d3c38a61127e0092aa3853ab77daeab
Jamie MacKillop <jamieronin@gmail.com>
2017-04-03 17:49:27 -0400

    Finish Algos done

--- algo/prime_number.rb
+++ algo/prime_number.rb
@@ -5,7 +5,14 @@
 # divisible by itself and the number 1.
 
 def is_prime?(number)
-
+  i = number - 1
+  while i > 2
+    if number % i == 0
+      return false
+    end
+    i -= 1
+  end
+  return true
 end
 
 # Second, implement the highest_prime_number_under method below. It should accept a number as
@@ -13,5 +20,15 @@ end
 # highest prime number under 10 is 7.
 
 def highest_prime_number_under(number)
-
+  i = number
+  while i > 2
+    if is_prime?(i)
+      return i
+    end
+    i -= 1
+  end
 end
+
+p highest_prime_number_under(10)
+p is_prime?(7)
+p is_prime?(10)
